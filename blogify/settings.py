76cbdd1e8dc29d7e39f1e80a5a4d91776714744c
manish079 <manishprajapat492@gmail.com>
2025-12-16 18:06:17 +0530

    cateogry by posts and 404 error custom

--- blogify/settings.py
+++ blogify/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0d-o02@((=#$hcqpnn&95d4y^m4piyvemb)xn#3ok1ns_w4@ds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blogs.context_processors.get_categories',
             ],
         },
     },
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 
-# Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+   
+    # Use get_object_or_404 when you want to show 404 error page if the category does not exist
+    category = get_object_or_404(Category, pk=category_id)
+    
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    
+    return render(request, 'posts_by_category.html', context)
