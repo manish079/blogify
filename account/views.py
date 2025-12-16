04258152efcfbd8cc321bbeedc6f43c025bf40e4
manish079 <manishprajapat492@gmail.com>
2025-12-16 16:55:50 +0530

    features post and recent articles

--- account/views.py
+++ account/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('updated_at')
+    posts = Blog.objects.filter(status="Published", is_featured=False).order_by('-updated_at')
     context = {
-        'categories': categories
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts
     }
     return render(request, 'home.html', context)
