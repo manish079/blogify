a858f1d0612ae27859e91ed6f11a1643f61e3e63
manish079 <manishprajapat492@gmail.com>
2025-12-16 20:01:44 +0530

    single blog post

--- blogify/settings.py
+++ blogify/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0d-o02@((=#$hcqpnn&95d4y^m4piyvemb)xn#3ok1ns_w4@ds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- blogify/urls.py
+++ blogify/urls.py
@@ -19,11 +19,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from account import views
 from django.conf import settings
+from blogs import views as BlogsView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name= 'home'),
     path('category/', include('blogs.urls')),
+    path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
+    
     
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
