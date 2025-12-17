48da97a1bd44732a3417bafd64d7fef7280fb95e
manish079 <manishprajapat492@gmail.com>
2025-12-17 20:06:51 +0530

    protected route

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
 
 
-
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
